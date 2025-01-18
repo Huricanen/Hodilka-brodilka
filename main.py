deb14b1799d4ce6532dac875205d9dbb33f1f379
Huricanen <rikex@yandex.ru>
2025-01-18 11:25:25 +0300

    Реализован класс предметов, которые можно собирать, сделана две гифки: 1 - монетка (type = 1) 2 - мешок с монетами (type = 2), 3 - алмаз. Реализован сбор предметов по касанию и увеличение количества очков.

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ for y in range(8):
             part.save(f'data/{y + 1}_{x + 1}.png')
 im.close()
 
-
 animations = {
     'idle_down': ['1_1.png', '1_2.png', '1_3.png'],
     'idle_left': ['2_1.png', '2_2.png', '2_3.png'],
@@ -96,6 +95,12 @@ class Hero(pygame.sprite.Sprite):
                         self.y += self.v
                     elif char_mid_y < rect_mid_y:
                         self.y -= self.v
+        rects_coll = [i.rect for i in collectibles]
+        collision_index_coll = self.rect.collidelist(rects_coll)
+        if collision_index_coll != -1:
+            self.score += collectibles.sprites()[collision_index_coll].cost
+            collectibles.remove(collectibles.sprites()[collision_index_coll])
+
         if self.moving:
             if self.v == 2:
                 self.anim_frames = 10
@@ -130,6 +135,17 @@ class Hero(pygame.sprite.Sprite):
             self.image = pygame.image.load(f'data/{animations[f'idle_{self.facing}'][img_index]}')
             screen.blit(self.image, self.rect)
 
+        self.draw_hud()
+
+    def draw_hud(self):
+        font = pygame.font.Font(None, 100)
+        text = font.render(f"{self.score}", True, (255, 0, 0))
+        text_x = 30
+        text_y = 530
+        text_w = text.get_width()
+        text_h = text.get_height()
+        screen.blit(text, (text_x, text_y))
+
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x1, y1, x2, y2):
@@ -152,8 +168,8 @@ class Collectible(pygame.sprite.Sprite):
     def __init__(self, x, y, type):
         super().__init__(all_sprites)
         self.image = None
-        self.w = 20
-        self.h = 20
+        self.w = 75
+        self.h = 75
         self.x = x
         self.y = y
         self.rect = pygame.Rect(x, y, self.w, self.h)
@@ -167,12 +183,14 @@ class Collectible(pygame.sprite.Sprite):
         elif type == 3:
             self.cost = 20
 
-        self.add(all_sprites)
+        self.add(collectibles)
 
-        self.gif = gif_pygame.load(f"data/{'coin' if self.type == 1 else 'diamond'}.gif")
+        self.gif = gif_pygame.load(f"data/{'coin' if self.type == 1 else ('money_bag'
+                                                                          if self.type == 2 else 'diamond')}.gif")
 
     def update(self):
-        self.gif.render(screen, (128 - self.gif.get_width() * 0.5, 256 - self.gif.get_height() * 0.5))
+        if self in collectibles:
+            self.gif.render(screen, (self.x, self.y))
 
 
 class Enemy:
@@ -197,12 +215,16 @@ if __name__ == '__main__':
     all_sprites = pygame.sprite.Group()
     all_sprites.add(main_character)
 
+    collectibles = pygame.sprite.Group()
+
     horizontal_borders = pygame.sprite.Group()
     vertical_borders = pygame.sprite.Group()
 
     w = Wall(251, 205, 270, 405)
     k = Wall(100, 300, 400, 310)
     c = Collectible(150, 40, 1)
+    f = Collectible(300, 40, 2)
+    v = Collectible(200, 80, 3)
 
     while running:
         screen.fill((255, 255, 255))
