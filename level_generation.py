0871cf4c941eaf48a5080a9c16a030e8d9a5b309
Huricanen <rikex@yandex.ru>
2025-02-03 21:07:28 +0300

    Сделана таблица лидеров и добавлено итоговое время на уровне на финальном экране.

--- level_generation.py
+++ level_generation.py
@@ -30,27 +30,48 @@ def start_level(level, hero, t):
         Collectible(200, 400, 1, camera_group)
         Enemy(600, 600, 1, 1000, hero, camera_group)
     if level == 2:
-        hero.x = 900
-        hero.y = 700
-        hero.rect.x = 900
-        hero.rect.y = 700
+        hero.x = 100
+        hero.y = 100
+        hero.rect.x = 100
+        hero.rect.y = 100
         hero.hp = 20
         hero.update()
         g1 = Wall(-500, 1000, 1920 + 500, 2000, camera_group)
         g2 = Wall(-500, -500, 1920 + 500, 0, camera_group)
         g3 = Wall(-1000, -500, 0, 1080 + 500, camera_group)
         g4 = Wall(1920, -500, 1920 + 1000, 1500, camera_group)
-        w = Wall(0, 205, 270, 210, camera_group)
-        k = Wall(200, 300, 205, 800, camera_group)
-        c = Collectible(150, 40, 1, camera_group)
-        f = Collectible(600, 300, 2, camera_group)
-        v = Collectible(1000, 80, 3, camera_group)
-        Collectible(1700, 400, 3, camera_group)
-        Collectible(200, 400, 1, camera_group)
-        Enemy(600, 300, 1, 300, hero, camera_group)
-        Enemy(800, 120, 1, 300, hero, camera_group)
-        Enemy(80, 200, 1, 300, hero, camera_group)
-        Enemy(500, 300, 1, 300, hero, camera_group)
+
+        w = Wall(0, 300, 350, 310, camera_group)
+        k = Wall(500, 0, 510, 800, camera_group)
+        Wall(500, 500, 1000, 510, camera_group)
+        Wall(990, 200, 1000, 500, camera_group)
+        Wall(700, 0, 710, 300, camera_group)
+        Wall(700, 800, 1500, 810, camera_group)
+        Wall(1200, 0, 1210, 800, camera_group)
+        Wall(300, 600, 310, 1080, camera_group)
+        Wall(700, 650, 710, 800, camera_group)
+        Wall(1690, 300, 1700, 800, camera_group)
+        Wall(1210, 300, 1700, 310, camera_group)
+        Wall(1400, 500, 1700, 510, camera_group)
+        s = Collectible(550, 100, 3, camera_group)
+        Collectible(100, 900, 3, camera_group)
+        Collectible(600, 900, 1, camera_group)
+        Collectible(630, 900, 1, camera_group)
+        Collectible(660, 900, 1, camera_group)
+        Collectible(690, 900, 1, camera_group)
+        Collectible(720, 900, 1, camera_group)
+        Collectible(800, 900, 1, camera_group)
+        Collectible(830, 900, 1, camera_group)
+        Collectible(860, 900, 1, camera_group)
+        Collectible(890, 900, 1, camera_group)
+        Collectible(920, 900, 1, camera_group)
+        Collectible(1450, 340, 3, camera_group)
+        Collectible(1500, 200, 2, camera_group)
+        Collectible(1420, 200, 1, camera_group)
+        Collectible(1550, 200, 1, camera_group)
+        Enemy(200, 900, 1, 500, hero, camera_group)
+        Enemy(700, 400, 1, 150, hero, camera_group)
+        Enemy(1730, 900, 1, 600, hero, camera_group)
     pygame.mixer.music.load('data/level_music.mp3')
     pygame.mixer.music.play()
     t = pygame.time.get_ticks()
--- screens.py
+++ screens.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import pygame
 from db_funcs import save_results_in_db
 from level_generation import start_level
@@ -20,6 +22,7 @@ def start_screen(hero, hud):
     height = screen.get_height()
     text1 = smallfont.render('Exit', True, color)
     text2 = smallfont.render('choose level', True, color)
+    text3 = smallfont.render('table of leaders', True, color)
     need1 = True
 
     while need1:
@@ -31,6 +34,9 @@ def start_screen(hero, hud):
                     pygame.quit()
                 elif width / 2 - 140 <= mouse[0] <= width / 2 + 140 and height / 2 + 50 <= mouse[1] <= height / 2 + 90:
                     return choose_level(hero, hud)
+                elif width / 2 - 140 <= mouse[0] <= width / 2 + 140 and height / 2 + 100 <= mouse[
+                    1] <= height / 2 + 140:
+                    top()
         screen.blit(img, (0, 0))
         mouse = pygame.mouse.get_pos()
         if width / 2 - 140 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
@@ -41,8 +47,13 @@ def start_screen(hero, hud):
             pygame.draw.rect(screen, color_light, [width / 2 - 140, height / 2 + 50, 280, 40])
         else:
             pygame.draw.rect(screen, color_dark, [width / 2 - 140, height / 2 + 50, 280, 40])
+        if width / 2 - 140 <= mouse[0] < width / 2 + 140 and height / 2 + 100 <= mouse[1] <= height / 2 + 140:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, height / 2 + 100, 280, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, height / 2 + 100, 280, 40])
         screen.blit(text1, (width / 2 - 25, height / 2))
         screen.blit(text2, (width / 2 - 75, height / 2 + 50))
+        screen.blit(text3, (width / 2 - 100, height / 2 + 100))
 
         pygame.display.update()
 
@@ -137,6 +148,8 @@ def finish(hero):
     text1 = smallfont.render('Congatulations!!!', True, color)
     text2 = smallfont.render('quit', True, color)
     text3 = smallfont.render(f'Your score: {hero.score}', True, (255, 0, 0))
+    text4 = smallfont.render(f'Your time on level:{(time_level_finished - time_level_started) // 1000}', True,
+                             (0, 255, 0))
     hero.score = 0
     level_chosen = 0
     time_level_started = 0
@@ -160,9 +173,10 @@ def finish(hero):
             pygame.draw.rect(screen, color_light, [width / 2 - 140, height / 2 + 100, 280, 40])
         else:
             pygame.draw.rect(screen, color_dark, [width / 2 - 140, height / 2 + 100, 280, 40])
-        screen.blit(text1, (width / 2 - 90, height / 2))
+        screen.blit(text1, (width / 2 - 100, height / 2 - 50))
         screen.blit(text2, (width / 2 - 25, height / 2 + 100))
-        screen.blit(text3, (width / 2 - 85, height / 2 + 50))
+        screen.blit(text3, (width / 2 - 70, height / 2 + 50))
+        screen.blit(text4, (width / 2 - 110, height / 2))
 
         pygame.display.update()
 
@@ -174,11 +188,66 @@ def finish(hero):
     return level_chosen, time_level_started, need_to_start_main_screen, need_to_quit_level
 
 
+def top():
+    """
+    Экран таблицы лидеров. Всего собирается топ из 5 игроков на каждый уровень. Если имя игрока больше, чем 10 символов,
+    то имя сокращается до 10 символов и ставится '...'.
+    """
+    img = pygame.image.load('data/backgrounds/bg5.png')
+    color = (255, 255, 255)
+    color_light = (170, 170, 170)
+    color_dark = (100, 100, 100)
+    width = screen.get_width()
+    smallfont = pygame.font.SysFont('Corbel', 35)
+    height = screen.get_height()
+    con = sqlite3.connect('data/user_data.db')
+    cur = con.cursor()
+    level_1_leaders = dict(enumerate(sorted(cur.execute("""SELECT num_id, level_1 FROM users_scores""").fetchall()[:5],
+                                            key=lambda x: (-eval(x[1])[0], -eval(x[1])[1]))))
+    level_2_leaders = dict(enumerate(sorted(cur.execute("""SELECT num_id, level_2 FROM users_scores""").fetchall()[:5],
+                                            key=lambda x: (-eval(x[1])[0], -eval(x[1])[1]))))
+    text_level_1_leaders = '\n'.join([f"{k + 1}: {f'{v[0][:10]}...' if len(v[0]) > 10 else v[0]} - {v[1]}" for k, v in
+                                      level_1_leaders.items()])
+    text_level_2_leaders = '\n'.join([f"{k + 1}: {f'{v[0][:10]}...' if len(v[0]) > 10 else v[0]} - {v[1]}" for k, v in
+                                      level_2_leaders.items()])
+
+    text1 = smallfont.render('Level №1', True, color)
+    text1_leaders = smallfont.render(text_level_1_leaders, True, color)
+    text2 = smallfont.render('Level №2', True, color)
+    text2_leaders = smallfont.render(text_level_2_leaders, True, color)
+    text3 = smallfont.render('Go back', True, color)
+
+    mouse = pygame.mouse.get_pos()
+    need5 = True
+    while need5:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                pygame.quit()
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if width / 2 - 140 <= mouse[0] <= width / 2 + 140 and height / 2 + 100 <= mouse[1] <= height / 2 + 140:
+                    need5 = False
+                    break
+        screen.blit(img, (0, 0))
+        mouse = pygame.mouse.get_pos()
+        if width / 2 - 140 <= mouse[0] < width / 2 + 140 and height / 2 + 100 <= mouse[1] <= height / 2 + 140:
+            pygame.draw.rect(screen, color_light, [width / 2 - 140, height / 2 + 100, 280, 40])
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2 - 140, height / 2 + 100, 280, 40])
+        screen.blit(text1, (width / 2 - 90 - 400, height / 2 - 200))
+        screen.blit(text1_leaders, (width / 2 - 90 - 400, height / 2 - 120))
+        screen.blit(text2, (width / 2 - 90 + 200, height / 2 - 200))
+        screen.blit(text2_leaders, (width / 2 - 90 + 200, height / 2 - 120))
+        screen.blit(text3, (width / 2 - 90 + 40, height / 2 + 100))
+
+        pygame.display.update()
+
+
 class Hud(pygame.sprite.Sprite):
     """
     Класс информации о счете. Хранит информацию о здоровье, времени, проведенном на уровне, и о очках, заработанных
     за сбор предметов.
     """
+
     def __init__(self, group, lc, t, hero):
         super().__init__(group)
         self.hero = hero
